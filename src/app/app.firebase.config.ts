0e4ca18138dc4750347af39a52b56643d713d5af
yan <ianromanenko@gmail.com>
2018-10-15 20:28:25 -0400

    Register added

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCGR2YRSw2NOnUHVadUKqMabjNXK0k2qzA",
+    authDomain: "miraclemovers-d901f.firebaseapp.com",
+    databaseURL: "https://miraclemovers-d901f.firebaseio.com",
+    projectId: "miraclemovers-d901f",
+    storageBucket: "miraclemovers-d901f.appspot.com",
+    messagingSenderId: "1007276234879"
+  };
+ 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,25 +10,29 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AngularFireModule } from 'angularfire2';
+import { FIREBASE_CONFIG } from './app.firebase.config';
+import { AngularFireAuthModule} from "angularfire2/auth";
+
 
 @NgModule({
   declarations: [
-    MyApp,
+    MyApp, 
     AboutPage,
     ContactPage,
-    HomePage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     AboutPage,
     ContactPage,
-    HomePage,
     TabsPage
   ],
   providers: [
