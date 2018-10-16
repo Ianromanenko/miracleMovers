2a673e9c3b320b79919bcd10ee15e838bd0aa987
yan <ianromanenko@gmail.com>
2018-10-15 22:07:02 -0400

    Login added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -22,6 +21,7 @@ import { AngularFireAuthModule} from "angularfire2/auth";
     AboutPage,
     ContactPage,
     TabsPage
+  
   ],
   imports: [
     BrowserModule,
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { User } from '../../models/user';
-
-
+import { AngularFireAuth } from "angularfire2/auth";
 
 @IonicPage()
 @Component({
@@ -13,11 +12,21 @@ export class LoginPage {
 
 	user = {} as User;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+  	private afAuth: AngularFireAuth,
+  	public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  login(){
-
+ async login(user: User){
+  	try{
+ 		const result = this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
+ 		if(result){
+ 			this.navCtrl.setRoot('HomePage')
+ 		}
+ 		} catch(e) {
+ 			console.error(e);
+ 		}
+  	
   }
 
   register(){
